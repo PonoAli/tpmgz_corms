e86b014bf478ad484c3bb6fa1eefc30c5e637a98
PonoAli <yark128@mail.ru>
2023-08-17 08:30:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './scss/app.scss';
+import Header from "./components/Header";
+import { Catigories } from "./components/Catigories";
+import { Sort } from "./components/Sort";
+import { PizzaBlock } from "./components/PizzaBlock";
+// import { CartItems } from "./components/CartItems";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+    <div className="content">
+      <div className="container">
+        <div className="content__top">
+          <Catigories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content_pizza-block">
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
 
